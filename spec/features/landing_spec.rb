5fa472b4acb81fce926c11c0a53dcebb73311011
Anthea Yur <acyur6@gmail.com>
2023-12-14 23:33:22 -0600

    Create stub for TN parks

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Landing Page" do
   before(:each) do
+    json_response = File.read('spec/fixtures/tn_parks.json')
+    stub_request(:get, "https://developer.nps.gov/api/v1/parks/?api_key=#{Rails.application.credentials.nps_gov[:key]}&stateCode=TN").
+                with(
+                  headers: {
+                  'Accept'=>'*/*',
+                  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                  'User-Agent'=>'Faraday v2.7.10'
+                  }).
+                to_return(status: 200, body: json_response, headers: {})
+
     visit root_path
   end
 
